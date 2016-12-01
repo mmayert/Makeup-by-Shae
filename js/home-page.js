c8ca27e0f9fa0759bbcf5ef9f9767d01e4603531
mmayert <madisonmayert@Madisons-MacBook-Pro-2.local>
2016-12-01 13:56:03 -0500

    colour scheme worked out

--- js/home-page.js
+++ js/home-page.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
      $(".toggle-menu").click(function(){
         $(".menu-primary").addClass("show-menu");
-        $(".toggle-menu").addClass("opacity-zero");
+        $(".fixed-menu").addClass("opacity-zero");
     });
     
     $(".close").click(function(){
         $(".menu-primary").removeClass("show-menu");
-        $(".toggle-menu").removeClass("opacity-zero");
+        $(".fixed-menu").removeClass("opacity-zero");
     });
    
 
