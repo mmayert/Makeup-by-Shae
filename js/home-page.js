168355166ccd6082cac06e0d4404543651fff237
mmayert <madisonmayert@Madisons-MacBook-Pro-2.local>
2016-12-04 11:50:03 -0500

    services complete

--- js/home-page.js
+++ js/home-page.js
@@ -1,14 +1,42 @@
 $(document).ready(function() {
-     $(".toggle-menu").click(function(){
-        $(".menu-primary").addClass("show-menu");
+        $(".toggle-menu").click(function(){
+        $("#menu-primary").addClass("show-menu");
         $(".fixed-menu").addClass("opacity-zero");
     });
     
     $(".close").click(function(){
-        $(".menu-primary").removeClass("show-menu");
+        $("#menu-primary").removeClass("show-menu");
         $(".fixed-menu").removeClass("opacity-zero");
     });
 });
+
+$(".prices-table-button").click (function(){
+    window.location.href = '#prices-table';    
+    });
+
+$(".go-to-images-event").click (function(){
+    window.location.href = 'portfolio.html';    
+    });
+
+$(".go-to-images-grad").click (function(){
+    window.location.href= 'portfolio.html';
+    });
+$(".go-to-images-bridal").click (function(){
+    window.location.href= 'portfolio.html';
+    });
+
+$(".go-to-images-editorial").click (function(){
+    window.location.href= 'portfolio.html';
+    });
+
+$(".complete-form-button").click (function() {
+    alert("Thanks for completeing this form! I'll be in contact with you as soon as possible!");
+    window.location.href= 'contact.html';
+});
+
+
+
+
 
 
 
