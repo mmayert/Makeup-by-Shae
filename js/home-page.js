1e52990298cd782b998a986e643a55a50dbe190a
mmayert <madisonmayert@Madisons-MacBook-Pro-2.local>
2016-12-04 12:51:53 -0500

    fix menu primary from id  back to class

--- js/home-page.js
+++ js/home-page.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
         $(".toggle-menu").click(function(){
-        $("#menu-primary").addClass("show-menu");
+        $(".menu-primary").addClass("show-menu");
         $(".fixed-menu").addClass("opacity-zero");
     });
     
     $(".close").click(function(){
-        $("#menu-primary").removeClass("show-menu");
+        $(".menu-primary").removeClass("show-menu");
         $(".fixed-menu").removeClass("opacity-zero");
     });
 });
