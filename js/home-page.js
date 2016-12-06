71d8be50eed524f3391b0bbc47eb194eecd797a0
mmayert <madisonmayert@Madisons-MacBook-Pro-2.local>
2016-12-05 20:31:37 -0500

    got the black background to move with java script

--- js/home-page.js
+++ js/home-page.js
@@ -3,13 +3,19 @@ $(document).ready(function() {
         $(".toggle-menu").click(function(){
         $(".menu-primary").addClass("show-menu");
         $(".fixed-menu").addClass("opacity-zero");
+        $(".fadded-background").addClass("show-menu");
     });
     
     $(".close").click(function(){
         $(".menu-primary").removeClass("show-menu");
         $(".fixed-menu").removeClass("opacity-zero");
+        $(".fadded-background").removeClass("show-menu")
     });
 });
+
+    $(".fadded-background").click(function(){
+        window.location.href= "index.html";
+    });
 
 
 /*Buttons*/
